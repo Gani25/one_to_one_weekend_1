89750a07d89df1ce1707c60ae318863fb266f08b
Gani25 <memonabdulgani3d@gmail.com>
2025-02-22 12:27:54 +0530

    Update InstructorController.java

--- src/main/java/com/sprk/one_to_one/controller/InstructorController.java
+++ src/main/java/com/sprk/one_to_one/controller/InstructorController.java
@@ -1,11 +1,10 @@
 package com.sprk.one_to_one.controller;
 
 import com.sprk.one_to_one.entity.Instructor;
+import com.sprk.one_to_one.entity.InstructorDetail;
 import com.sprk.one_to_one.repository.InstructorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class InstructorController {
@@ -22,5 +21,55 @@ public class InstructorController {
         Instructor instructorSaved = instructorRepository.save(instructor);
 
         return instructorSaved;
+    }
+    @PutMapping("/instructor/{insId}")
+    public Instructor updateInstructor(@PathVariable int insId, @RequestBody Instructor updatedInstructor) {
+        Instructor dbInstructor = instructorRepository.findById(insId).orElse(null);
+        InstructorDetail dbInstructorDetail = dbInstructor.getInstructorDetail(); // Db Detail
+        InstructorDetail newInstructorDetail = updatedInstructor.getInstructorDetail(); // From API Call
+        if(dbInstructor != null) {
+            // Logic For Update
+            if(updatedInstructor.getFirstName() != null && !updatedInstructor.getFirstName().isBlank()){
+                dbInstructor.setFirstName(updatedInstructor.getFirstName());
+            }
+            if(updatedInstructor.getLastName() != null && !updatedInstructor.getLastName().isBlank()){
+                dbInstructor.setLastName(updatedInstructor.getLastName());
+            }
+            if(updatedInstructor.getPhone() != null && !updatedInstructor.getPhone().isBlank()){
+                dbInstructor.setPhone(updatedInstructor.getPhone());
+            }
+
+            if(newInstructorDetail != null){
+
+                if (dbInstructorDetail != null)
+                {
+                    if(newInstructorDetail.getHobby() != null && !newInstructorDetail.getHobby().isBlank()){
+                        dbInstructorDetail.setHobby(newInstructorDetail.getHobby());
+                    }
+                    if (newInstructorDetail.getEducation() != null && !newInstructorDetail.getEducation().isBlank()){
+                        dbInstructorDetail.setEducation(newInstructorDetail.getEducation());
+                    }
+
+
+                    // dbInstructorDetail will be having detail id so this will also update
+                    dbInstructor.setInstructorDetail(dbInstructorDetail);
+                }else{
+                    dbInstructor.setInstructorDetail(newInstructorDetail);
+                }
+
+
+
+            }
+
+            // Update
+            // db instructor will be having id so it will update
+            Instructor postUpdatedInstructor = instructorRepository.save(dbInstructor);
+
+            return postUpdatedInstructor;
+
+        }
+        else {
+            return null;
+        }
     }
 }
